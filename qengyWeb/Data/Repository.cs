90ae07c1fa7b394609b108449f64c3700bfdc09a
mr-blazor <74933779+mr-blazor@users.noreply.github.com>
2022-03-20 22:05:00 +0100

    Data repositories

---
+++ qengyWeb/Data/Repository.cs
@@ -0,0 +1,11 @@
+﻿namespace qengyWeb.Data
+{
+    public class Repository
+    {
+        public string Repo { get; set; }
+        public string Description { get; set; }
+        public string GIT { get; set; }
+        public string Demo { get; set; }
+
+    }
+}
